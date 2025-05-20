6668c1e53874898b0a0e6d228c5afe0134b8e2a7
dhdhfkk1119 <whwhwjddn123@gmail.com>
2025-05-20 15:43:05 +0900

    first commit

---
+++ src/until/DataBaseUtil.java
@@ -0,0 +1,21 @@
+package until;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Getter
+@Setter
+public class DataBaseUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/usedphone?serverTimezone=Asia/Seoul";
+    private static final String DB_USER = "root";
+    private static final String DB_PWD = "asd1234";
+
+    // 데이터벵스 연결 객체를 변환 하는 함수
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL,DB_USER,DB_PWD);
+    }
+}
