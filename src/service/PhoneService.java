e482fe854322025bfa546b550cbdee0121338129
lsssmin <tmxjel77777@naver.com>
2025-05-21 12:46:26 +0900

    서비스 패키지 추가

---
+++ src/service/PhoneService.java
@@ -0,0 +1,19 @@
+package service;
+
+import dao.PhoneDAO;
+
+public class PhoneService {
+
+    private final PhoneDAO phoneDAO= new PhoneDAO();
+
+    // 중고폰을 추가하는 서비스
+
+
+
+    // 중고폰 전체 조회 하는 서비스
+    public List<PhoneDAO> getAllPhone() throws SQLException {
+        return PhoneDAO.getAllPhone();
+    }
+
+    // 특정 기종만 조회 하는 서비스
+}
